0a580ce846375df23ab25063e52895d0603df10e
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2021-07-31 09:44:30 +0200

    first js

---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+//const elements = ingredients.map((elements) => {
+// const ingredientsItem = document.createElement('li');
+// return ingredientsItem;
+//});
+
+//const ingredientsList = document.createElement("li");
+//ingredientsList.textContent = "Картошка";
+
+//ingredients.appendChild(ingredientsList);
+//console.log(ingredients);
+
