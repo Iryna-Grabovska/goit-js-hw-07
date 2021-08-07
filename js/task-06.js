5de28b5b1a48b50f29169449f5d6af69d704af61
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2021-08-07 11:30:52 +0200

    add task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,20 @@
 
-
-
 const input = document.querySelector('[data-length]')
 
 console.log(input);
 
 input.addEventListener('blur', (event) => {
   console.log(event);
-  input.style.borderColor("valid") = ;
+
+
+  let value = input.value.length;
+
+  if (value != input) {
+    input.classList.add('invalid');
+    input.classList.remove('valid');
+    return;
+  }
+  input.classList.add('valid');
+  input.classList.remove('invalid');
+
 })
